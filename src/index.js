eb23bec8399edacedd3dbae2142ba1032e2ce916
Toma Otani <otani.toma@gmail.com>
2020-05-06 00:02:19 -0700

    useReducer

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import AppUseReducer from './components/AppUseReducer';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <AppUseReducer />
   </React.StrictMode>,
   document.getElementById('root')
 );
